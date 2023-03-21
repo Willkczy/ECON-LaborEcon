913d866c48f6f55bdd37ce6179e6f6d142dd4279
黃泓愷 <willkczy@huanghongkaideMacBook-Pro.local>
2023-03-21 12:18:55 +0800

    push Roy Model R file

--- PSFD.R
+++ PSFD.R
@@ -11,11 +11,13 @@ data_2 = data %>%
 data_2$a05 = ifelse(data_2$a05 == 1 | data_2$a05 == 2, 1, data_2$a05)
 data_2$a05 = ifelse(data_2$a05 == 3, 0, data_2$a05)
 
-work_mean = data_2 %>% 
+data_2 = data_2 %>% 
   group_by(a02a01) %>%
   summarise(work_mean = mean(a05))
 
-scatter_plot = ggplot(work_mean, aes(x = a02a01, y = work_mean)) + 
-  geom_point()
+data_2 = data_2 %>% rename(work_dummy_mean = work_mean, age = a02a01)
+
+scatter_plot = ggplot(data_2) + 
+  geom_point(mapping = aes(x = age, y = work_dummy_mean))
 
 scatter_plot
--- Roy_Model.R
+++ Roy_Model.R
@@ -1,6 +1,40 @@
+library(MASS)
+library(data.table)
+library(magrittr)
+library(dplyr)
+
+set.seed(0)
+
+#1
 # set μ0, μ1, σ0, σ1, σ01, C
 mu_0 = 2
 mu_1 = 5
-sigma_0 = 3
-sigma_1 = 5
-set.seed(0)
+sigma_0 = sqrt(7)
+sigma_1 = sqrt(5)
+sigma_01 = 3.5
+cost = 5
+
+sizes = 10000000
+
+#2
+multi_normal_data = as.data.table(mvrnorm(n = sizes,
+                                         mu = c(0, 0), 
+                                         Sigma = matrix(c(sigma_1, sigma_0, sigma_01, sigma_01), ncol = 2)))
+multi_normal_data = multi_normal_data[, .(epsilon_0 = V1, epsilon_1 = V2)]
+
+#3
+multi_normal_data = multi_normal_data[, .(w_0 = mu_0 + epsilon_0, w_1 = mu_1 + epsilon_1), by=.(epsilon_0, epsilon_1)]
+
+#4
+multi_normal_data = multi_normal_data[, `:=` (I = sample(c(0,1), replace = TRUE,size = sizes))]
+
+#5
+#Q1
+multi_normal_data %>%
+  group_by(I) %>%
+  summarise(Q_0 = mean(w_0))
+
+#Q2
+multi_normal_data %>%
+  group_by(I) %>%
+  summarise(Q_1 = mean(w_1))
